27530d80e64cf4e489551585b4e0104f9a73804e
ForgeSnacks <community@haemmerl.it>
2026-07-05 17:24:46 +0200

    Release v0.3.0

--- custom_components/vertragsmanager/config_flow.py
+++ custom_components/vertragsmanager/config_flow.py
@@ -5,6 +5,7 @@ import voluptuous as vol
 
 from homeassistant import config_entries
 from homeassistant.core import callback
+from homeassistant.helpers import selector
 
 from .const import (
     CATEGORIES,
@@ -21,19 +22,165 @@ from .const import (
     DOMAIN,
 )
 
-DATA_SCHEMA = vol.Schema(
-    {
-        vol.Required(CONF_NAME): str,
-        vol.Required(CONF_CATEGORY, default=CATEGORIES[-1]): vol.In(CATEGORIES),
-        vol.Required(CONF_PROVIDER): str,
-        vol.Required(CONF_COST): vol.Coerce(float),
-        vol.Required(CONF_CYCLE, default=CYCLES[0]): vol.In(CYCLES),
-        vol.Required(CONF_START_DATE): str,
-        vol.Required(CONF_NOTICE_DAYS, default=30): vol.Coerce(int),
-        vol.Required(CONF_DURATION_MONTHS, default=12): vol.Coerce(int),
-        vol.Required(CONF_AUTO_RENEW, default=True): bool,
-    }
-)
+
+def _build_user_schema(user_input=None):
+    user_input = user_input or {}
+
+    return vol.Schema(
+        {
+            vol.Required(
+                CONF_NAME,
+                default=user_input.get(CONF_NAME, vol.UNDEFINED),
+            ): selector.TextSelector(
+                selector.TextSelectorConfig(
+                    type=selector.TextSelectorType.TEXT,
+                )
+            ),
+            vol.Required(
+                CONF_CATEGORY,
+                default=user_input.get(CONF_CATEGORY, CATEGORIES[-1]),
+            ): selector.SelectSelector(
+                selector.SelectSelectorConfig(
+                    options=CATEGORIES,
+                    mode=selector.SelectSelectorMode.DROPDOWN,
+                )
+            ),
+            vol.Required(
+                CONF_PROVIDER,
+                default=user_input.get(CONF_PROVIDER, vol.UNDEFINED),
+            ): selector.TextSelector(
+                selector.TextSelectorConfig(
+                    type=selector.TextSelectorType.TEXT,
+                )
+            ),
+            vol.Required(
+                CONF_COST,
+                default=user_input.get(CONF_COST, vol.UNDEFINED),
+            ): selector.NumberSelector(
+                selector.NumberSelectorConfig(
+                    min=0,
+                    step=0.01,
+                    mode=selector.NumberSelectorMode.BOX,
+                )
+            ),
+            vol.Required(
+                CONF_CYCLE,
+                default=user_input.get(CONF_CYCLE, CYCLES[0]),
+            ): selector.SelectSelector(
+                selector.SelectSelectorConfig(
+                    options=CYCLES,
+                    mode=selector.SelectSelectorMode.DROPDOWN,
+                )
+            ),
+            vol.Required(
+                CONF_START_DATE,
+                default=user_input.get(CONF_START_DATE, vol.UNDEFINED),
+            ): selector.TextSelector(
+                selector.TextSelectorConfig(
+                    type=selector.TextSelectorType.DATE,
+                )
+            ),
+            vol.Required(
+                CONF_NOTICE_DAYS,
+                default=user_input.get(CONF_NOTICE_DAYS, 30),
+            ): selector.NumberSelector(
+                selector.NumberSelectorConfig(
+                    min=0,
+                    step=1,
+                    mode=selector.NumberSelectorMode.BOX,
+                )
+            ),
+            vol.Required(
+                CONF_DURATION_MONTHS,
+                default=user_input.get(CONF_DURATION_MONTHS, 12),
+            ): selector.NumberSelector(
+                selector.NumberSelectorConfig(
+                    min=1,
+                    step=1,
+                    mode=selector.NumberSelectorMode.BOX,
+                )
+            ),
+            vol.Required(
+                CONF_AUTO_RENEW,
+                default=user_input.get(CONF_AUTO_RENEW, True),
+            ): selector.BooleanSelector()
+        }
+    )
+
+
+def _build_options_schema(current: dict):
+    return vol.Schema(
+        {
+            vol.Required(
+                CONF_CATEGORY,
+                default=current.get(CONF_CATEGORY, CATEGORIES[-1]),
+            ): selector.SelectSelector(
+                selector.SelectSelectorConfig(
+                    options=CATEGORIES,
+                    mode=selector.SelectSelectorMode.DROPDOWN,
+                )
+            ),
+            vol.Required(
+                CONF_PROVIDER,
+                default=current.get(CONF_PROVIDER, vol.UNDEFINED),
+            ): selector.TextSelector(
+                selector.TextSelectorConfig(
+                    type=selector.TextSelectorType.TEXT,
+                )
+            ),
+            vol.Required(
+                CONF_COST,
+                default=current.get(CONF_COST, vol.UNDEFINED),
+            ): selector.NumberSelector(
+                selector.NumberSelectorConfig(
+                    min=0,
+                    step=0.01,
+                    mode=selector.NumberSelectorMode.BOX,
+                )
+            ),
+            vol.Required(
+                CONF_CYCLE,
+                default=current.get(CONF_CYCLE, CYCLES[0]),
+            ): selector.SelectSelector(
+                selector.SelectSelectorConfig(
+                    options=CYCLES,
+                    mode=selector.SelectSelectorMode.DROPDOWN,
+                )
+            ),
+            vol.Required(
+                CONF_START_DATE,
+                default=current.get(CONF_START_DATE, vol.UNDEFINED),
+            ): selector.TextSelector(
+                selector.TextSelectorConfig(
+                    type=selector.TextSelectorType.DATE,
+                )
+            ),
+            vol.Required(
+                CONF_NOTICE_DAYS,
+                default=current.get(CONF_NOTICE_DAYS, 30),
+            ): selector.NumberSelector(
+                selector.NumberSelectorConfig(
+                    min=0,
+                    step=1,
+                    mode=selector.NumberSelectorMode.BOX,
+                )
+            ),
+            vol.Required(
+                CONF_DURATION_MONTHS,
+                default=current.get(CONF_DURATION_MONTHS, 12),
+            ): selector.NumberSelector(
+                selector.NumberSelectorConfig(
+                    min=1,
+                    step=1,
+                    mode=selector.NumberSelectorMode.BOX,
+                )
+            ),
+            vol.Required(
+                CONF_AUTO_RENEW,
+                default=current.get(CONF_AUTO_RENEW, True),
+            ): selector.BooleanSelector()
+        }
+    )
 
 
 class VertragsmanagerConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
@@ -58,7 +205,9 @@ class VertragsmanagerConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 )
 
         return self.async_show_form(
-            step_id="user", data_schema=DATA_SCHEMA, errors=errors
+            step_id="user",
+            data_schema=_build_user_schema(user_input),
+            errors=errors,
         )
 
     @staticmethod
@@ -75,21 +224,21 @@ class VertragsmanagerOptionsFlow(config_entries.OptionsFlow):
 
     async def async_step_init(self, user_input=None):
         current = {**self.config_entry.data, **self.config_entry.options}
-
-        schema = vol.Schema(
-            {
-                vol.Required(CONF_CATEGORY, default=current.get(CONF_CATEGORY)): vol.In(CATEGORIES),
-                vol.Required(CONF_PROVIDER, default=current.get(CONF_PROVIDER)): str,
-                vol.Required(CONF_COST, default=current.get(CONF_COST)): vol.Coerce(float),
-                vol.Required(CONF_CYCLE, default=current.get(CONF_CYCLE)): vol.In(CYCLES),
-                vol.Required(CONF_START_DATE, default=current.get(CONF_START_DATE)): str,
-                vol.Required(CONF_NOTICE_DAYS, default=current.get(CONF_NOTICE_DAYS)): vol.Coerce(int),
-                vol.Required(CONF_DURATION_MONTHS, default=current.get(CONF_DURATION_MONTHS)): vol.Coerce(int),
-                vol.Required(CONF_AUTO_RENEW, default=current.get(CONF_AUTO_RENEW)): bool,
-            }
-        )
+        errors: dict[str, str] = {}
 
         if user_input is not None:
-            return self.async_create_entry(title="", data=user_input)
+            try:
+                from datetime import date
 
-        return self.async_show_form(step_id="init", data_schema=schema)
+                date.fromisoformat(user_input[CONF_START_DATE])
+            except ValueError:
+                errors["base"] = "invalid_date"
+
+            if not errors:
+                return self.async_create_entry(title="", data=user_input)
+
+        return self.async_show_form(
+            step_id="init",
+            data_schema=_build_options_schema(current if user_input is None else user_input),
+            errors=errors,
+        )
